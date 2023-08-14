e9e9c7951df381e0b27ca961a9852eda722bcb9a
arhip14 <kruvdyk1503@gmail.com>
2023-08-14 09:37:01 +0300

    intial-commit-4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'; 
 import './index.css';
+import 'normalize.css'; 
 
 const root = document.getElementById('root');
 const rootElement = ReactDOM.createRoot(root);
